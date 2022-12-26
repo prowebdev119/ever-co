8c81fa8e744f763693976fc943c3413b1de3c0cf
prowebdev110 <tieosong119@gmail.com>
2022-12-27 01:40:32 +0900

    Update auth.controller.ts

--- packages/core/src/controllers/auth/auth.controller.ts
+++ packages/core/src/controllers/auth/auth.controller.ts
@@ -1,6 +1,3 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('auth')
 @Controller('auth')
